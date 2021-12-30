4e384643a19275c03765b0de18bdd848a5d8b745
AlexShumy <91432456+AlexShumy@users.noreply.github.com>
2021-12-30 22:40:38 +0200

    Add files via upload

---
+++ Dicee/Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Alex Shumylo on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
